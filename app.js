52055b47a8adbe11505a32d1e2885d147cf06393
abdollatiif <abdollatiif@gmail.com>
2014-07-19 17:03:09 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ var express = require('express');
 
 var app = module.exports = express.createServer();
 
+app.use(require('less-middleware')(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
+
 var port = process.env.PORT || 3000;
 
 app.listen(port, function() {
