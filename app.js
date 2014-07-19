87b2dd043e4fbd06b5946048f3b362906ccda829
abdollatiif <abdollatiif@gmail.com>
2014-07-19 16:24:08 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,15 @@
-var express = require('express'),
-    connect = require('connect');
+var express = require('express');
 
 var app = module.exports = express.createServer();
 
 var port = process.env.PORT || 3000;
-
-app.configure('development', function() {
-    app.use(connect.static('./public'));
-    app.set('appIndex', './public/app.html');
-});
 
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
 
 app.get('/', function(req, res) {
-  res.sendfile(app.set('appIndex'));
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
 });
 
