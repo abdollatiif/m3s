e09b850897b1a1d978a62f8738609b958d750fb8
abdollatiif <abdollatiif@gmail.com>
2014-07-23 04:21:36 +0000

    retry

--- public/app.js
+++ public/app.js
@@ -56,7 +56,7 @@ Ext.application({
         Ext.fly('appLoadingIndicator').destroy();
 
         // Initialize the main view
-        Ext.Viewport.add(Ext.create('m3s.view.Main'));
+        Ext.Viewport.add(Ext.create('m3s.view.List'));
     },
 
     onUpdated: function() {
