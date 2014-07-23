61072176b0a7c87ec645eddb631caeb5dba7c918
abdollatiif <abdollatiif@gmail.com>
2014-07-23 14:28:05 +0000

    Add list directly on main view

--- public/app/view/Main.js
+++ public/app/view/Main.js
@@ -1,47 +1,17 @@
 Ext.define('m3s.view.Main', {
 	
-    extend: 'Ext.tab.Panel',
+    extend: 'Ext.Container',
     
     xtype: 'main',
     
     requires: [
-        'Ext.TitleBar',
         'm3s.view.ListObj'
     ],
     
     config: {
-        tabBarPosition: 'bottom',
-
         items: [
             {
-                title: 'Objects',
-                
-                iconCls: 'home',
-
-                styleHtmlContent: true,
-                
-                scrollable: true,
-
-                items: { 
-                	  xtype: 'lsobjs'
-                }
-               
-            },
-            {
-                title: 'Notes',
-                
-                iconCls: 'action',
-
-                items: [
-                    {
-                        docked: 'top',
-                        xtype: 'titlebar',
-                        title: 'List of Notes'
-                    },
-                    {
-                    	html: '<p>listObjects</p>'
-                    }
-                ]
+                xtype: 'lsobjs'
             }
         ]
     }
