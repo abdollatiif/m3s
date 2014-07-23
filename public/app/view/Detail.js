1d985be270641ad94c8f2a8b4c5f914526e74d52
abdollatiif <abdollatiif@gmail.com>
2014-07-23 20:40:46 +0000

    Add Dialog & Detail files

---
+++ public/app/view/Detail.js
@@ -0,0 +1,36 @@
+Ext.define('m3s.view.Detail', {
+
+    extend: 'Ext.Container',
+    
+    xtype: 'objectDetail',
+    
+	config: {
+
+        scrollable: 'vertical',
+
+        items: [
+            {
+                docked: 'top',
+                xtype: 'titlebar',
+                cls: 'small',
+                items: [
+                    {
+                        xtype: 'button',
+                        cls: 'backBtn',
+                        id: 'objectBackButton',
+                        align: 'left'
+                    },
+                    {
+                        xtype: 'button',
+                        cls: 'shareBtn',
+                        iconCls: 'shareBtn',
+                        id: 'objectShareButton',
+                        align: 'right'
+                    }
+                ]
+            }
+        ],
+        
+        tpl: Ext.create('Ext.XTemplate','{idsc}')
+    }
+});
