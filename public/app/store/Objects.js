b07997d032ee256d5d4c2fa8e45aa9028214a740
abdollatiif <abdollatiif@gmail.com>
2014-07-25 04:07:19 +0000

    add record in proxy reader

--- public/app/store/Objects.js
+++ public/app/store/Objects.js
@@ -12,7 +12,8 @@ Ext.define('m3s.store.Objects', {
 
             reader: {
                 type: 'json',
-                rootProperty: 'objects.json'
+                record: 'json',
+                rootProperty: 'objects'
             }
         },
         
