190633660c821f47c413407d3fe8e2723867272e
abdollatiif <abdollatiif@gmail.com>
2014-07-27 04:35:25 +0000

    re

--- public/app/view/Detail.js
+++ public/app/view/Detail.js
@@ -35,13 +35,7 @@ Ext.define('m3s.view.Detail', {
         tpl: Ext.create('Ext.XTemplate',
         		'<div>{content}</div>',
         		'</br>',
-        		'<div class="fb-comments" data-href="http://m3s.herokuapp.com" data-numposts="3" data-colorscheme="dark">',
-        			'<fb:comments num_posts="5" width="620" mobile="false" notify="true" href="http://m3s.herokuapp.com" fb-xfbml-state="rendered" class="fb_iframe_widget">',
-        				'<span style="height: 252px; width: 620px;">',
-        					'<iframe id="f87914cb8" name="f257d8f97" scrolling="no" title="Facebook Social Plugin" class="fb_ltr" src="http://m3s.herokuapp.com/comments" style="border: none; overflow: hidden; height: 252px; width: 620px;"></iframe>',
-        				'</span>',
-        			'</fb:comments>',
-        		'</div>'        
+        		'<fb:comments href="http://example.com/comments" numposts="5" colorscheme="dark"></fb:comments>'        
         )
     }
 });
