bd2b4c53610615b0fb988a94d88ee94c2bd5647e
abdollatiif <abdollatiif@gmail.com>
2014-07-31 05:14:11 +0100

    add json fields in model

--- public/app/model/Grocery.js
+++ public/app/model/Grocery.js
@@ -1,9 +1,15 @@
 Ext.define('m3s.model.Grocery', {
     extend: 'Ext.data.Model',
     config: {
-        fields: [{
-            name: 'text',
-            type: 'string'
-        }]
+        fields: [
+            {
+            	name: 'text',
+            	type: 'string'
+        	},
+        	{
+        		name: 'json',
+        		type: 'string'
+        	}
+        ]
     }
 });
