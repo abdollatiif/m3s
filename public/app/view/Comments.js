235f470eac2c4150782c19cd2f0c1d0573d9d1e3
abdollatiif <abdollatiif@gmail.com>
2014-08-01 01:11:17 +0100

    add cls value

--- public/app/view/Comments.js
+++ public/app/view/Comments.js
@@ -7,19 +7,17 @@ Ext.define('m3s.view.Comments', {
     ],
 
     config: {
-        //baseCls: Ext.baseCSSPrefix + 'sheet',
-        modal: true,
+
+    	modal: true,
         centered : false,
         hideOnMaskTap : true,
-
-        //ui: 'detail',
+        
+        cls:'float-panel',
 
         width: 400,
         top: 0,
         bottom: 0,
         right: 0,
-
-        loan: null,
 
         layout: {
             type: 'vbox',
@@ -32,8 +30,7 @@ Ext.define('m3s.view.Comments', {
                 flex: 1,
                 items: [
                     { html : 'Item 1', style: 'background-color: #5E99CC' },
-                    { html : 'Item 2', style: 'background-color: #759E60' },
-                    { html : 'Item 3' }
+                    { html : 'Item 2', style: 'background-color: #759E60' }
                 ]
             }
         ]
