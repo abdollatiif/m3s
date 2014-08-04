326b3fb7ac479d1f62282e06446a278196998cba
abdollatiif <abdollatiif@gmail.com>
2014-08-04 17:54:01 +0100

    getStore

--- public/app/controller/Objects.js
+++ public/app/controller/Objects.js
@@ -245,7 +245,7 @@ Ext.define('m3s.controller.Objects', {
     },
     
     onPlusTap: function(record,e,eOpts){
-    	console.log(this.getNotes().getActiveItem());
+    	console.log(this.getNotes().getActiveItem().getStore());
     }
     
 });
