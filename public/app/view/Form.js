989a4fd1796379c4ac00f64c84246bb413d8e4f5
abdollatiif <abdollatiif@gmail.com>
2014-08-04 23:41:21 +0100

    Add form view

---
+++ public/app/view/Form.js
@@ -0,0 +1,33 @@
+Ext.define('m3s.view.Form', {
+	
+    extend: 'Ext.form.Panel',
+    	
+    xtype: 'objectForm',
+    
+    config: {
+    	
+    	items: [{
+            xtype: 'fieldset',
+            items: [
+                {
+                    xtype: 'textfield',
+                    name : 'name',
+                    label: 'Name'
+                },
+                {
+                    xtype: 'emailfield',
+                    name : 'email',
+                    label: 'Email'
+                },
+                {
+                    xtype: 'passwordfield',
+                    name : 'password',
+                    label: 'Password'
+                }
+            ]
+        }],
+        
+        record: this.getRecord()
+    }
+    	
+});
