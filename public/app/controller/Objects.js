a30d9b7d2ed7a26f0197d6b837b2274f3dcdd5f1
abdollatiif <abdollatiif@gmail.com>
2014-08-06 18:49:00 +0100

    re

--- public/app/controller/Objects.js
+++ public/app/controller/Objects.js
@@ -332,10 +332,10 @@ Ext.define('m3s.controller.Objects', {
     			        seq: m3s.LastNodeSeq
     			    },
     			    success: function(response){
-    			    	if (!response.responseText){
+    			    	if (response.responseText=='null'){
     			    		seq = m3s.LastNodeSeq + 1;
     			    	}else{
-    			    		console.log(typeof(response.responseText));
+    			    		console.log(response.responseText);
     			    	}
     			    }
     			});
