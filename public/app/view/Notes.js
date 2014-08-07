c893e2979ca28a035581b02d2c4930dc908da8e2
abdollatiif <abdollatiif@gmail.com>
2014-08-07 16:08:05 +0100

    getNode()

--- public/app/view/Notes.js
+++ public/app/view/Notes.js
@@ -60,7 +60,7 @@ Ext.define('m3s.view.Notes', {
 			},
 			
 			activeitemchange: function(self, value, oldValue, eOpts ){
-				console.log(value);
+				console.log(value.getStore().getNode());
 			}
         }
     }
